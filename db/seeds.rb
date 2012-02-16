8f1422d64057673d7d5fd2dc76d66a942731aae2
Jonas Lundqvist <lundqvist.j@gmail.com>
2012-02-16 21:33:24 +0100

    seed some currencies
    euro, dollar and kronor

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+# encoding: UTF-8
+
+sek = Currency.create(code: 'SEK', name: 'Svenska kronor', locale: 'sv-SE')
+eur = Currency.create(code: 'EUR', name: 'Euro', locale: 'de')
+usd = Currency.create(code: 'USD', name: 'US Dollar', locale: 'en')
