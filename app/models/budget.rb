592ec581f8e55c6420da93608e0806ce7346b953
Jonas Lundqvist <lundqvist.j@gmail.com>
2012-02-25 10:17:25 +0100

    build periods on budget creation

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -11,7 +11,22 @@ class Budget < ActiveRecord::Base
   attr_accessor :starting_at
   columns_hash["starting_at"] = ActiveRecord::ConnectionAdapters::Column.new("starting_at", nil, "date")
   
+  after_create :create_periods
+  
   validates :name, :presence => true
   validates :interval, :presence => true
   validates :account_ids, :presence => true
+  
+  private
+  def create_periods
+    period_start = self.starting_at
+    i = 1
+    while period_start < Date.today
+      next_start = period_start + self.interval.to_i.months
+      period_end = next_start - 1.day
+      period = self.budget_periods.create(start_date: period_start, end_date: period_end, position: i)
+      period_start = next_start
+      i += 1
+    end
+  end
 end
