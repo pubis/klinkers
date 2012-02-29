b95fe4f87a9f7441f3be54c40e74c736533f9370
Jonas Lundqvist <lundqvist.j@gmail.com>
2012-02-29 22:59:10 +0100

    adds spending table and chart to category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,36 @@ class CategoriesController < ApplicationController
 
   def index
   end
+  
+  def show
+    @accounts = current_user.accounts.users.no_payees
+    account_ids = params[:accounts] || @accounts.map { |a| a.id }
+
+    start_date = Date.today.beginning_of_year
+
+    @months = []
+    while start_date < Date.today
+      item = TransactionItem.all(
+        :select => "SUM(transaction_items.amount) AS amount",
+        :joins => ["LEFT JOIN transactions ON (transactions.id = transaction_items.transaction_id)"],
+        :conditions => [
+          "transaction_items.account_id IN (?) AND transaction_items.category_id = ? AND transactions.operation_date > ? AND transactions.operation_date <= ?",
+          account_ids,
+          params[:id],
+          start_date,
+          start_date.end_of_month
+        ],
+        :group => "transaction_items.category_id"
+      ).first
+      @months << {
+        title: "#{Date::MONTHNAMES[start_date.month].titleize} #{start_date.year}",
+        start_date: start_date,
+        end_date: start_date.end_of_month,
+        amount: item.nil? ? 0.0 : item.amount.abs
+      }
+      start_date += 1.month
+    end
+  end
 
   def new
     if params[:parent] && parent = current_user.categories.find(params[:parent])
