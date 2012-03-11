714b0d65721b37afbadc6568f8bce1b6d08fb195
Jonas Lundqvist <lundqvist.j@gmail.com>
2012-03-11 10:48:39 +0100

    clean up locale detection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,29 +24,18 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   
   def detect_and_set_locale
-    locale = detect_locale
-    logger.debug(locale)
-    I18n.locale = locale
+    I18n.locale = detect_locale
   end
   
   def detect_locale
     available = LANGUAGES.map { |l| l[:locale] }
-    logger.debug "Available locales: #{available.inspect}"
-    logger.debug "User pref locales: #{request.user_preferred_languages}"
-    
     if current_user && available.include?(current_user.locale)
-      logger.debug "Current u locale: #{current_user.locale}"
       locale = current_user.locale
     else
-      
       if params[:lang] && available.include?(params[:lang])
-        logger.debug "Parm lang locale: #{params[:lang]}"
         locale = params[:lang]
       else
         locale = request.preferred_language_from(available)
-
-        logger.debug "Pref from locales: #{locale}"
-        logger.debug "Comp from locales: #{request.compatible_language_from(available)}"
       end
     end
     locale
