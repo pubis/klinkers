1eb07e847a98a17c82df3d5cfed449f8202736f6
Jonas Lundqvist <lundqvist.j@gmail.com>
2012-03-15 19:41:51 +0100

    Adds title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+  def title(page_title)
+    content_for(:title) { page_title }
+  end
+  
   def flash_class(level)
     case level
     when :notice then "alert alert-success"
