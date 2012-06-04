e46db94ea563a76a429238289ca28f092c867396
Jonas Lundqvist <lundqvist.j@gmail.com>
2012-06-04 15:04:13 +0200

    preload more sti children

--- config/initializers/preload_sti_models.rb
+++ config/initializers/preload_sti_models.rb
@@ -1,5 +1,9 @@
 if Rails.env.development?
-  %w[payee portfolio].each do |c|
+  %w[payee portfolio loan credit_card].each do |c|
+    require_dependency File.join("app","models","#{c}.rb")
+  end
+
+  %w[transfer withdrawal deposit].each do |c|
     require_dependency File.join("app","models","#{c}.rb")
   end
 end
