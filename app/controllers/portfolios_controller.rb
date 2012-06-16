53bbace852e236c78b57309df6918762ca8696c6
Jonas Lundqvist <lundqvist.j@gmail.com>
2012-06-16 17:34:23 +0200

    add_investment action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -6,4 +6,23 @@ class PortfoliosController < ApplicationController
   def show
     @portfolio = current_user.portfolios.find(params[:id])
   end
+
+  def add_investment
+    @portfolio = current_user.portfolios.find(params[:id])
+
+    if params[:symbol].present?
+      yahoo_quote_lookup = YahooStock::Quote.new(stock_symbols: params[:symbol], read_parameters: [:symbol, :name])
+      stock = yahoo_quote_lookup.results(:to_hash).output.first
+      logger.debug("Y: #{yahoo_quote_lookup}\nS: #{stock}")
+
+      raise "Symbol not found" if stock[:symbol].nil?
+
+      investment = Investment.find_or_create_by_symbol_and_name(stock[:symbol], stock[:name])
+      @portfolio.investments << investment
+
+      redirect_to @portfolio, :notice => "Investment #{investment[:name]} added!"
+    else
+      raise "No stock symbol present"
+    end
+  end
 end
