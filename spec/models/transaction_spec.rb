d00d019807ad93f2d006e2a9de9fd7263ab3a6a7
Jonas Lundqvist <lundqvist.j@gmail.com>
2012-06-21 00:24:30 +0200

    use transaction factory in transaction spec

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -3,10 +3,8 @@ require 'spec_helper'
 describe Transaction do
   describe ".occured_during" do
     it "should only return transactions that occured during specified period" do
-      t1 = Transaction.new(operation_date: "2012-02-19")
-      t1.save(validate: false)
-      t2 = Transaction.new(operation_date: "2012-02-20")
-      t2.save(validate: false)
+      t1 = FactoryGirl.create(:transaction, operation_date: "2012-02-19")
+      t2 = FactoryGirl.create(:transaction, operation_date: "2012-02-20")
 
       Transaction.occured_during("2012-01-01".to_date.."2012-01-31".to_date).should eq([])
       Transaction.occured_during("2012-02-01".to_date.."2012-02-29".to_date).should include(t1, t2)
