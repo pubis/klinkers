3889819e0b01b08ae98d748a24b7749991703235
Jonas Lundqvist <lundqvist.j@gmail.com>
2012-09-28 21:51:25 +0200

    autoload /lib

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module Klinkers
     # Deep-load model directory
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
 
+    config.autoload_paths += %W(#{config.root}/lib)
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
