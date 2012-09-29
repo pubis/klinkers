409a2359813775d531a494c2c6737b911d778dc8
Jonas Lundqvist <lundqvist.j@gmail.com>
2012-09-29 17:47:39 +0200

    remove debug code

--- lib/locale_setter.rb
+++ lib/locale_setter.rb
@@ -14,28 +14,23 @@ module LocaleSetter
   end
 
   def self.by_default(default = I18n.default_locale)
-    l_debug('default', default)
     default
   end
 
   def self.by_params(params)
-    l_debug('params', params[:locale])
     locale = params[:locale]
     locale if locale_available?(locale)
   end
 
   def self.by_session(session)
-    l_debug('session', session[:locale])
     session[:locale]
   end
 
   def self.by_user(user)
-    l_debug('user', user.locale)
     user.locale
   end
 
   def self.by_http(accepts)
-    l_debug('http', (accept_list(accepts) & I18n.available_locales).first)
     (accept_list(accepts) & I18n.available_locales).first
   end
 
@@ -47,9 +42,5 @@ module LocaleSetter
 
   def self.accept_list(accepts)
     accepts.downcase.scan(/([\w-]{2,})/).map { |l| l.first.to_sym }
-  end
-
-  def self.l_debug(method, locale)
-    Rails.logger.debug("Setting locale by #{method} to #{locale}")
   end
 end
